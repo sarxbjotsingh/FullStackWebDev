216ede97e303aff587082618f53d72d89be48ef7
sarxbjotsingh <sarxbjotsingh@gmail.com>
2025-04-03 13:16:57 +0530

    learnt nodejs nodemon command

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,5 +1,8 @@
 console.log("hello world");
 console.log("hello world");
+console.log("hello world");
+// you can use npx nodemon index.js to run the file and it will automatically restart when you make changes to the file
+// you can also use npx node index.js to run the file without nodemon
 const fs = require("fs");
 fs.readFile("test.txt", "utf8", (err, data) => {
   if (err) throw err;
